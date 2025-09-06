35d2bb47072a21b9a6d8492b5f4c5e4d0bcbd011
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2025-09-06 11:49:25 -0300

    refactor: updating svg files

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,14 +1,3 @@
-// declare module "*.svg" {
-//   import { FC, SVGProps } from "react";
-//   const content: FC<SVGProps<SVGElement>>;
-//   export default content;
-// }
-
-// declare module "*.svg?url" {
-//   const content: any;
-//   export default content;
-// }
-
 declare module "*.svg" {
   import * as React from "react";
   const ReactComponent: React.FunctionComponent<
